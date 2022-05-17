4198cfb492e657ec85d3f53e633c7dc1dc88f112
J Cody Eisenbach <ceisenbach@gmail.com>
2022-05-16 20:07:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Grid_Fix_Lead_Gen
+
+Simple Web App using a Node and ExpressJS combo for the backend with Nodemailer to send mail and Bootstrap with Flexbox for the HTML front end
+
+HTML was chosen over REACT for SEO purposes
+
+Deployed app @ https://www.gridfixtexas.com
---
+++ index.js
@@ -0,0 +1,51 @@
+$(function () {
+  $("#pls-btn").on("click", function () {
+    setTimeout(function () {
+      $(".card").addClass("paused");
+    }, 350);
+    //remove old event classes
+    $(".explore").removeClass("fadeIn");
+    $(".card-title").removeClass("fadeOut");
+    $(".card-text").removeClass("fadeOut");
+    $(".card-btn").removeClass("fadeOut");
+    $(".card-title").removeClass("hidden");
+    $(".card-text").removeClass("hidden");
+    $(".card-btn").removeClass("hidden");
+    $(".u-div").removeClass("hidden");
+    $(".card").removeClass("hidden");
+    $(".m-close").removeClass("hidden");
+
+    //add new event classes
+    $(".explore").addClass("fadeOut");
+    $(".card-title").addClass("fadeIn");
+    $(".card-text").addClass("fadeIn");
+    $(".card-btn").addClass("fadeIn");
+    $(".u-div").addClass("fadeIn");
+    $(".m-close").addClass("fadeIn");
+    $(".card").addClass("transform-active");
+    $("#pls-btn").addClass("hidden");
+
+    //close events
+    $(".m-close").on("click", function () {
+      $(".card-title").removeClass("fadeIn");
+      $(".card-text").removeClass("fadeIn");
+      $(".card-btn").removeClass("fadeIn");
+      $(".u-div").removeClass("fadeIn");
+      $(".explore").addClass("fadeIn");
+      $(".card-title").addClass("hidden");
+      $(".card-text").addClass("hidden");
+      $(".card-btn").addClass("hidden");
+      $(".u-div").addClass("hidden");
+      $(".m-close").addClass("hidden");
+      $("#pls-btn").removeClass("hidden");
+      $(".card").removeClass("paused");
+
+      // reset element containing animation
+      setTimeout(function () {
+        $(".card").removeClass("transform-active");
+        $(".card").addClass("hidden");
+        void $(".card").offsetWidth;
+      }, 350);
+    });
+  });
+});
